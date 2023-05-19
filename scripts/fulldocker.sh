d87a6985ed60dbf5cdc339f19313864164de17a2
parthmvpatel20 <52366659+parthmvpatel20@users.noreply.github.com>
2023-05-19 21:34:28 +0530

    Create fulldocker.sh

---
+++ scripts/fulldocker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo docker stop msextension
+sudo docker rm msextension
+sudo docker rmi 622361742002.dkr.ecr.us-east-1.amazonaws.com/test:latest
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 622361742002.dkr.ecr.us-east-1.amazonaws.com
+sudo docker pull 622361742002.dkr.ecr.us-east-1.amazonaws.com/test:latest
+
+sudo docker run -dit --restart always --network host  -v /var/www:/app/logs --name msextension 622361742002.dkr.ecr.us-east-1.amazonaws.com/test:latest
