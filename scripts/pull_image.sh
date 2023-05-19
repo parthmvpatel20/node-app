897f04352aef4d140f409d58d498a43db4706011
parthmvpatel20 <52366659+parthmvpatel20@users.noreply.github.com>
2023-05-19 18:20:11 +0530

    Add files via upload

---
+++ scripts/pull_image.sh
@@ -0,0 +1,2 @@
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 622361742002.dkr.ecr.us-east-1.amazonaws.com
+sudo docker pull 622361742002.dkr.ecr.us-east-1.amazonaws.com/test:latest
